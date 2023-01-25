5f1f698f1e8034d5fe01deb31a317503f2ac0c0c
AlinaDorosh-dev <alina.dorosh@gmail.com>
2023-01-25 20:29:31 +0100

    first day practice

--- backend/models/loginModel.js
+++ backend/models/loginModel.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+
 const loginSchema = new Schema({
   email: {
     type: String,
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,21 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import MainPage from "./views/MainPage";
+import RegisterPage from "./views/RegisterPage";
+import ErrorPage from "./views/ErrorPage";
+import CalendarPage from "./views/CalendarPage";
 function App() {
+  
   return (
+    
     <Router>
     
     <Routes>
-      <Route path='/' element={<MainPage />} />
+      <Route index element={<Navigate replace to="/login" />} />
       <Route path='/login' element={<MainPage />} />
-      {/* <Route path='/signup' element={<RegisterPage/>} /> */}
-      {/* <Route path='/*' element={<ErrorPage />} /> */}
+      <Route path='/signup' element={<RegisterPage/>} />
+      <Route path='/calendar' element={<CalendarPage/>} />
+      <Route path='/*' element={<ErrorPage />} />
     </Routes>
   </Router>
   );
